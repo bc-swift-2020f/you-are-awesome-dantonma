d2e6f07554e2fc85b4a696fe39e9a57e8e62ab54
Marissa D'Antonio <dantonma@bc.edu>
2020-09-13 13:37:47 -0400

    2.7 Completed

--- You are awesome!/ViewController.swift
+++ You are awesome!/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageLabel.text = ""
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
